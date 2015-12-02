8b03692a617063106a5cfc9f44b56591ed5b7a2b
kristinsif <kristinsifv@hotmail.com>
2015-12-02 00:18:53 +0000

    bætti við commentum á functionir í classes (.h skrár)

--- Programmers.h
+++ Programmers.h
@@ -11,16 +11,33 @@ class Programmers
 {
     public:
         Programmers();
+
+          // input : nr - nr of the programmer
+          // returns : programmer at position nr
         Programmer getProgrammer(int nr);
+
+          // returns : count of programmers
         int size();
-        void read();
+
+          // input : p - programmer to be added
+          // programmer p as been added to the programmers list
         void add(Programmer p);
+
+          // programmer at nr has been deleted from the programmers list
         void del(int nr);
 
-        // returns the numbers of all programmers that matches the search criteria
+          // input : v - vector<int>
+          //         sSearch - search criteria
+          // v includes nr of all programmers that fullfill the search criteria
         void Find(vector<int>& pr, string sSearch);
+
+          // sorts the programmers by name
         void sortByName();
+
+          // sorts the programmers by gender+name
         void sortByGender();
+
+          // saves the programmers to disk
         void save();
     private:
         vector<Programmer> programmers;
--- ppInterface.h
+++ ppInterface.h
@@ -7,11 +7,27 @@ class ppInterface
 {
     public:
         ppInterface();
+
+          // ask user for search string and displayes all programmers that are in the search criteria
         void search();
+
+          // input : sSearch - search criteria
+          // displayes all programmers that are in the search criteria
         void display(string sSearch);
+
+          // reads info about the new programmer that the user puts inn and adds to the list
         void addProgrammer();
+
+          // saves all programmers to disk
         void save();
+
+          // asks for nr of the programmer to delete and removes him from the list
         void deleteProgrammer();
+
+          // input  : type=1 sorts by name
+          //          type=2 sorts by gender
+          //          else sorts by name
+          // sorts the programmer list in given order
         void sortProgrammers(int type);
     private:
         Programmers programmers;
